fd79ddf1ee8a1e5af01be65b6eb55061c6a65cd0
Puzz <minhboys123@gmail.com>
2021-12-20 03:41:11 +0700

    Thêm thanh menu trên cùng bằng masterpage

---
+++ userpage/layouts/top_bar_user.php
@@ -0,0 +1,14 @@
+        <div class="top-bar">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <i class="fa fa-envelope"></i>
+                        daotao@stu.edu.vn
+                    </div>
+                    <div class="col-sm-6">
+                        <i class="fa fa-phone-alt"></i>
+                        +0902.992.306
+                    </div>
+                </div>
+            </div>
+        </div>
