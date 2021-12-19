57332e33a40d96fa88cf5a6ed64e8b116aac5156
Puzz <minhboys123@gmail.com>
2021-12-20 03:35:33 +0700

    Thêm thanh menu cuối bằng masterpage

---
+++ userpage/layouts/bottom_bar_user.php
@@ -0,0 +1,31 @@
+        <div class="bottom-bar">
+            <div class="container-fluid">
+                <div class="row align-items-center">
+                    <div class="col-md-3">
+                        <div class="logo">
+                            <a href="index.php">
+                                <img src="img/logo.png" alt="Logo">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="search">
+                            <input type="text" placeholder="Search">
+                            <button><i class="fa fa-search"></i></button>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="user">
+                            <a href="wishlist.php" class="btn wishlist">
+                                <i class="fa fa-heart"></i>
+                                <span>(5)</span>
+                            </a>
+                            <a href="cart.php" class="btn cart">
+                                <i class="fa fa-shopping-cart"></i>
+                                <span>(4)</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
