bcdf8aa13d34acbf1ce2a995d7051f90c29afb7b
Puzz <minhboys123@gmail.com>
2021-12-20 03:11:10 +0700

    Tạo phần menu bên trái bằng masterpage

---
+++ adminpage/layouts/menuadmin.php
@@ -0,0 +1,53 @@
+<aside id="left-panel" class="left-panel">
+    
+        <nav class="navbar navbar-expand-sm navbar-default">
+            <div id="main-menu" class="main-menu collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="dashboard.php"><i class="menu-icon fa fa-laptop"></i>Dashboard </a>
+                    </li>
+                    
+                    <li class="menu-item-has-children active dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa  fa-users"></i>Users</a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><i class="fa fa-users"></i><a href="userpage.php">Users Manager</a></li>
+                            <li><i class="fa fa-users"></i><a href="usercreatepage.php">Create Users</a></li>
+                           
+                        </ul>
+                    </li>
+                    <li class="menu-item-has-children active dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa  fa-shopping-cart"></i>Products</a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><i class="menu-icon fa  fa-shopping-cart"></i><a href="productpage.php">Products Manager</a></li>
+                            <li><i class="menu-icon fa  fa-shopping-cart"></i><a href="productcreatepage.php">Create Products</a></li>
+                           
+                        </ul>
+                    </li>
+               
+                    <li>
+                        <a href="categorypage.php"> <i class="menu-icon ti-menu-alt"></i>Category </a>
+                    </li>
+                    
+                    <li>
+                        <a href="invoicepage.php"> <i class="menu-icon ti-money"></i>Invoice </a>
+                    </li>
+                    
+                    <li>
+                        <a href="postpage.php"> <i class="menu-icon ti-pencil-alt"></i>Post</a>
+                    </li>
+
+                    <li>
+                        <a href="postcategorypage.php"> <i class="menu-icon ti-align-justify"></i>Post Category </a>
+                    </li>                                                            
+                   
+                </ul>
+            </div>
+                     <!-- /.navbar-collapse -->
+        </nav>
+    </aside>
+
+
+
+
+
+
