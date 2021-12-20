5e1cfcb445042724ed0bda327d2e92ebf3fe5388
Puzz <minhboys123@gmail.com>
2021-12-20 12:48:56 +0700

    Thêm chức năng hiển th danh sáchịsản phẩm mơ ước

---
+++ userpage/wishlist.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include "layouts/header_user.php" ?>
+    </head>
+
+    <body>
+        <!-- Top bar Start -->
+        <?php include 'layouts/top_bar_user.php'; ?>
+        <!-- Top bar End -->
+        
+        <!-- Nav Bar Start -->
+        <?php  include "layouts/menu_user.php" ?> 
+        <!-- Nav Bar End -->      
+        
+        <!-- Bottom Bar Start -->
+        <?php include 'layouts/bottom_bar_user.php'; ?>
+        <!-- Bottom Bar End --> 
+        
+        <!-- Breadcrumb Start -->
+        <div class="breadcrumb-wrap">
+            <div class="container-fluid">
+                <ul class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item"><a href="#">Products</a></li>
+                    <li class="breadcrumb-item active">Wishlist</li>
+                </ul>
+            </div>
+        </div>
+        <!-- Breadcrumb End -->
+        
+        <!-- Wishlist Start -->
+        <div class="wishlist-page">
+            <div class="container-fluid">
+                <div class="wishlist-page-inner">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="table-responsive">
+                                <table class="table table-bordered">
+                                    <thead class="thead-dark">
+                                        <tr>
+                                            <th>Product</th>
+                                            <th>Price</th>
+                                            <th>Quantity</th>
+                                            <th>Add to Cart</th>
+                                            <th>Remove</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="align-middle">
+                                        <tr>
+                                            <td>
+                                                <div class="img">
+                                                    <a href="#"><img src="img/product-6.jpg" alt="Image"></a>
+                                                    <p>RADIUS JEANS</p>
+                                                </div>
+                                            </td>
+                                            <td>$190</td>
+                                            <td>
+                                                <div class="qty">
+                                                    <button class="btn-minus"><i class="fa fa-minus"></i></button>
+                                                    <input type="text" value="1">
+                                                    <button class="btn-plus"><i class="fa fa-plus"></i></button>
+                                                </div>
+                                            </td>
+                                            <td><button class="btn-cart">Add to Cart</button></td>
+                                            <td><button><i class="fa fa-trash"></i></button></td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <div class="img">
+                                                    <a href="#"><img src="img/product-7.jpg" alt="Image"></a>
+                                                    <p>CASHMERE HERRINGBONE COAT</p>
+                                                </div>
+                                            </td>
+                                            <td>$599</td>
+                                            <td>
+                                                <div class="qty">
+                                                    <button class="btn-minus"><i class="fa fa-minus"></i></button>
+                                                    <input type="text" value="1">
+                                                    <button class="btn-plus"><i class="fa fa-plus"></i></button>
+                                                </div>
+                                            </td>
+                                            <td><button class="btn-cart">Add to Cart</button></td>
+                                            <td><button><i class="fa fa-trash"></i></button></td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <div class="img">
+                                                    <a href="#"><img src="img/product-8.jpg" alt="Image"></a>
+                                                    <p>PAVIA MESH TOP</p>
+                                                </div>
+                                            </td>
+                                            <td>$230</td>
+                                            <td>
+                                                <div class="qty">
+                                                    <button class="btn-minus"><i class="fa fa-minus"></i></button>
+                                                    <input type="text" value="1">
+                                                    <button class="btn-plus"><i class="fa fa-plus"></i></button>
+                                                </div>
+                                            </td>
+                                            <td><button class="btn-cart">Add to Cart</button></td>
+                                            <td><button><i class="fa fa-trash"></i></button></td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <div class="img">
+                                                    <a href="#"><img src="img/product-9.jpg" alt="Image"></a>
+                                                    <p>VANILAR SWEATER</p>
+                                                </div>
+                                            </td>
+                                            <td>$165</td>
+                                            <td>
+                                                <div class="qty">
+                                                    <button class="btn-minus"><i class="fa fa-minus"></i></button>
+                                                    <input type="text" value="1">
+                                                    <button class="btn-plus"><i class="fa fa-plus"></i></button>
+                                                </div>
+                                            </td>
+                                            <td><button class="btn-cart">Add to Cart</button></td>
+                                            <td><button><i class="fa fa-trash"></i></button></td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <div class="img">
+                                                    <a href="#"><img src="img/product-10.jpg" alt="Image"></a>
+                                                    <p>WHITE DRESS</p>
+                                                </div>
+                                            </td>
+                                            <td>$124</td>
+                                            <td>
+                                                <div class="qty">
+                                                    <button class="btn-minus"><i class="fa fa-minus"></i></button>
+                                                    <input type="text" value="1">
+                                                    <button class="btn-plus"><i class="fa fa-plus"></i></button>
+                                                </div>
+                                            </td>
+                                            <td><button class="btn-cart">Add to Cart</button></td>
+                                            <td><button><i class="fa fa-trash"></i></button></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Wishlist End -->
+        
+        <!-- Footer Start -->
+        <?php include 'layouts/footer_user.php'; ?> 
+        <!-- Footer End -->
+        
+        <!-- Footer Bottom Start -->
+            <?php include 'layouts/footer_bottom_user.php' ; ?>
+        <!-- Footer Bottom End -->       
+        
+        <!-- Back to Top -->
+        <?php include 'layouts/back_to_top_user.php'; ?>
+        
+        <!-- JavaScript Libraries -->
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+        <script src="lib/easing/easing.min.js"></script>
+        <script src="lib/slick/slick.min.js"></script>
+        
+        <!-- Template Javascript -->
+        <script src="js/main.js"></script>
+    </body>
+</html>
